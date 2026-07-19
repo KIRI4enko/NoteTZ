defbfe6b693efc3f0aaeed3f63ea5c4fb4c93794
KIRI4enko <efimenko_kirill05@mail.ru>
2026-07-19 11:06:32 +0300

    Создание моделей

--- Note/models.py
+++ Note/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=30, 
+        unique=True,
+        verbose_name="Название тега"
+    )
+    
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+
+    def __str__(self):
+        return self.name
+    
+
+class Note(models.Model):
+    title = models.CharField(
+        max_length=100, 
+        verbose_name="Заголовок заметки"
+    )
+
+    text = models.TextField(
+        verbose_name="Текст заметки"
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="notes",
+        blank=True,
+        verbose_name="Теги"
+    )
+
+    created_ad = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+
+    class Meta:
+        verbose_name = "Заметка"
+        verbose_name_plural = "Заметки"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+ 
